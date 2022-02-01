9d7ffe54b3e2583df058f6911010548940b4b8e8
manum4n <mvelasquez@hyasesores.pe>
2022-02-01 11:04:09 -0500

    add: consumo concors

--- GP_Prueba backend/src/AspNetClient/Startup.cs
+++ GP_Prueba backend/src/AspNetClient/Startup.cs
@@ -28,7 +28,7 @@ namespace AspNetClient
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -42,6 +42,12 @@ namespace AspNetClient
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:4200");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
